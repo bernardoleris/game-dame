d887e52f1882011521a4b90cf5942c60cab6c80d
Bernardo <bernardo.leris1@gmail.com>
2024-05-06 15:16:08 -0300

    iniciando register page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Info from './pages/Info/Info.jsx';
+import Register from './pages/Register/Register.jsx'
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart.jsx";
 import NoMatch from "./pages/NoMatch.jsx"
 import { Routes, Route } from "react-router-dom";
@@ -7,7 +8,8 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Info />}></Route> 
+      <Route path='/' element={<Register />}></Route> 
+      <Route path='/info' element={<Info />}></Route> 
       <Route path='/shoppingcart' element={<ShoppingCart />}></Route> 
       <Route path="*" element={<NoMatch />}></Route> 
     </Routes>
